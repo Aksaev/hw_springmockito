e50428be9342c56c27cdc56f75dbd207f042ea27
Aksaev <mail-ax@ya.ru>
2023-05-18 20:22:23 +0400

    Initial commit

---
+++ src/main/java/me/aksaev/hw_springmockito/HwSpringmockitoApplication.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_springmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringmockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringmockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/aksaev/hw_springmockito/HwSpringmockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_springmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringmockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
